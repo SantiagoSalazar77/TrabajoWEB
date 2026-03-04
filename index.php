e88d57127df37bb692b6cbd0769c36aa0f3078fa
SantiagoSalazar77 <santiago1.salazar@ucp.edu.co>
2026-03-04 17:05:06 -0500

    Add HTML structure to index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <title>Página Web</title>
+</head>
+<body>
+    <div id="cabeza">
+        <img src="img/cabeza.png" />
+    </div>
+    <div id="chica">
+        <img src="img/chica.png" />
+    </div>
+    <div id="cabeza2">
+        <img src="img/cabeza2.png">
+    </div>
+    <div id="pie">
+        <img src="img/pie.png">
+    </div>
+    <div id="lupa">
+        <img src="img/lupa.png">
+    </div>
+    <div id="pie2">
+        <img src="img/pie2.png">
+    </div>
+</body>
+</html>
